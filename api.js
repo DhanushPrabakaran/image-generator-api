3467ed328d222ebfa0af209038a68136c87347c9
dhanush <dhanushprabakaran18@gmail.com>
2023-06-01 10:53:13 +0530

    chages

--- api.js
+++ api.js
@@ -26,7 +26,11 @@ app.options(cors());
 //     );
 //     next();
 //   });
-                                           
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}          
+app.options(cors(corsOptions));                     
 const clientPromise = mongoClient.connect();
 
   const agg = [
