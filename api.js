a3e6d6065740e6865c0e381062aeca113d8f6731
dhanush <dhanushprabakaran18@gmail.com>
2023-06-03 16:21:57 +0530

    chnges

--- api.js
+++ api.js
@@ -2,33 +2,20 @@ const express = require ('express');
 const app = express();
 const router = express.Router();
 
-// const serverless = require ('serverless-http');
 const bodyParser = require ('body-parser');
-// const { MongoClient } = require ('mongodb');
 
 const { MongoClient, ObjectId } = require('mongodb');
-//const id = new ObjectID();
-// var mongodb = require("mongodb");
-// var ObjectID = require('mongodb').ObjectID;
-//var objectId = new ObjectID();
+
 const url = 'mongodb+srv://Dhanush:SD18A2004@cluster0.2s94ek1.mongodb.net/';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const mongoClient = new MongoClient(url);
 const cors = require("cors");      
 app.use(express.json({limit: '50mb'}));
-// app.options(cors()); 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*"); 
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-//   });
+
 var corsOptions = {
   origin: '*',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));                     
 const clientPromise = mongoClient.connect();
@@ -79,18 +66,14 @@ router.post('/post',async(req,res)=>{
 
 router.post('/login',async(req,res)=>{
   try{
-
-    //let dat = JSON.parse(req);
     const database = (await clientPromise).db("mini_project");
     const collection =await database.collection("users");
     const results = await collection.find({"email":req.body.email,"password":req.body.password}).toArray();
     
 
     if(results.length>0){
-      //console.log(dat);
-     // res.send(JSON.stringify(results));
+
      res.send({statusCode: 500, body:JSON.stringify(results) });
-     //res.send({statusCode: 200, body: "valid Credentials"});
       }else{
         res.send({statusCode: 500, body: "Invalid Credentials"});
       
@@ -128,19 +111,7 @@ router.post('/get/user',async(req,res)=>
   try{
   let body=req.body;
   let text = body.search;
-    // const agg = [
-    //     {
-    //       '$search': {
-    //         'index': 'default', 
-    //         'text': {
-    //           'query': `${text}`, 
-    //           'path': {
-    //             'wildcard': '*'
-    //           }
-    //         }
-    //       }
-    //     }
-    //   ];  
+ 
     const database = (await clientPromise).db("mini_project");
     const collection =database.collection("image_generator");
 
